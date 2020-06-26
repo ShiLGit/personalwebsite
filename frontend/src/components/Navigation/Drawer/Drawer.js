d2d43e19152eaa64d1837e9ea974e13a22db0f61
lilianshi00@gmail.com <lilianshi00@gmail.com>
2020-06-26 14:56:51 -0400

    hamburger animation

--- frontend/src/components/Navigation/Drawer/Drawer.js
+++ frontend/src/components/Navigation/Drawer/Drawer.js
@@ -6,7 +6,7 @@ import Hamburger from './DrawerElements/Hamburger';
 const Drawer = (props)=>{
     return(
         <div style ={{backgroundColor:'red'}}>       
-            <Hamburger clickHandler={props.toggleDrawer}/>
+            <Hamburger clickHandler={props.toggleDrawer} active ={props.drawerOpen}/>
             <DrawerOptions drawerOpen={props.drawerOpen} onClickHandler={props.onClickHandler}/>
         </div>
     )
--- frontend/src/components/Navigation/Drawer/DrawerElements/Hamburger.js
+++ frontend/src/components/Navigation/Drawer/DrawerElements/Hamburger.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './Hamburger.module.css';
 const Hamburger = (props)=>{
+
     return(
-        <div className ={styles.Wrapper} onClick={props.clickHandler}>
+        <div className ={styles.Wrapper} >
             <h1>LILIAN SHI</h1>
-            <div className={styles.ButtonWrapper}>    
-                <div className={styles.LineStyle}></div>
-                <div className={styles.LineStyle}></div>
-                <div className={styles.LineStyle}></div>
+            <div className={styles.ButtonWrapper} onClick={props.clickHandler}>    
+                <div className={`${styles.LineStyle}` + (props.active? ` ${styles.TopLine}`:"")}></div>
+                <div className={`${styles.LineStyle}` + (props.active? ` ${styles.MiddleLine}`:"")}></div>
+                <div className={`${styles.LineStyle}` + (props.active? ` ${styles.BottomLine}`:"")}></div>
             </div>
         </div>
     )
