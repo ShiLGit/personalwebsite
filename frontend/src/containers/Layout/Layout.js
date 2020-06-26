d742aa78036ea08320d719e1be1e35c5ae2ce251
lilianshi00@gmail.com <lilianshi00@gmail.com>
2020-06-26 15:36:07 -0400

    image resize

--- frontend/src/containers/Layout/Layout.js
+++ frontend/src/containers/Layout/Layout.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react';
 import classes from './Layout.module.css';
-import Accordions from '../Accordions/Accordions';
 import Navbar from '../../components/Navigation/Navbar/Navbar';
-import Heading from '../../components/Decoration/Heading';
-import portrait from '../../pictures/vectorhead.png';
 import Drawer from '../../components/Navigation/Drawer/Drawer';
 
 import Landing from '../Pages/Landing/Landing';
