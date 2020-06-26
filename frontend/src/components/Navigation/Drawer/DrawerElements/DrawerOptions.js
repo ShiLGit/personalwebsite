dabf53bab9769bad4cdc68817e0d6442d60d9cfd
lilianshi00@gmail.com <lilianshi00@gmail.com>
2020-06-26 15:17:04 -0400

    make drawer components disappear on screen resize to >1000px

--- frontend/src/components/Navigation/Drawer/DrawerElements/DrawerOptions.js
+++ frontend/src/components/Navigation/Drawer/DrawerElements/DrawerOptions.js
@@ -4,16 +4,15 @@ import Backdrop from '../../../UI/Backdrop';
 const DrawerOptions = (props)=>{
     console.log(props)
     return(
-    <React.Fragment>
+<div className={styles.All}>
     <div className = {styles.Wrapper} style={{display: props.drawerOpen?'block':'none'}}>
         <h3>About</h3>
         <h3>Projects</h3>
         <h3>Suggestion Box</h3>
         <h3>Email</h3>
     </div>
-    <Backdrop zIndex={50} show={props.drawerOpen} onClickHandler={props.onClickHandler}/>
-
-    </React.Fragment>
+    <Backdrop zIndex={50} show={props.drawerOpen} onClickHandler={props.onClickHandler} />
+</div>
     )
 }
 
