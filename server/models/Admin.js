dc9512dc7815ca58100b6286137b84511f4674ad
lilianshi00@gmail.com <lilianshi00@gmail.com>
2020-07-05 15:15:45 -0400

    logins save token to db

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
+const config = require('../config');
+const jwt = require('jsonwebtoken');
 
+console.log('this??', this);
 const adminSchema = new mongoose.Schema({
     username: {type: String, required: true},
     password: {type: String, required: true},
     tokens: [{type: String, required: true}]
 })
+
 
 const Admin = mongoose.model('Admin', adminSchema);
 module.exports = Admin;
--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -1,23 +1,40 @@
 const authRouter = require('express').Router();
-const loginData = require('../AUTH_DATA');
 const jwt = require('jsonwebtoken');
 const config = require('../config.js');
+
 const auth = require('../middleware/auth');
-
+const Admin = require('../models/Admin');
 //RUN AUTHENTICATION MIDDLEWARE
-authRouter.route('/login').post(auth, (req, res)=>{
+authRouter.route('/login').post( async (req, res)=>{
+    const loginData = req.body;
+    try{
+        const adminInstance = await Admin.findOne({username: loginData.username, password: loginData.password});
+        if(!adminInstance){
+            throw new Error('Unmatching PW');
+        }
 
+        const token = jwt.sign({_id: adminInstance.username}, config.JWT_SECRET);
+        console.log(adminInstance);
+        adminInstance.tokens = adminInstance.tokens.concat(token);
+        adminInstance.save();
+        res.send({token});
+    }catch(e){
+        console.log(e);
+        res.status(201).send({fail: "Login failure: could not authenticate."});
+    }
+    /* 
     if(!req.body){
         return res.status(400).send({error: "Undefined login body."});
     }
     
     if(req.body.username === loginData.username && req.body.password === loginData.password){
         //generate token
+        const admin = 
         const token = jwt.sign({_id: req.body.username}, config.JWT_SECRET, {expiresIn: '3h'});
         res.status(201).send({username: req.body.username, token});
     }else{
         res.status(201).send({fail: "Login failure: username and password incorrect.", token: null});
-    }
+    }*/
 
 })
 
