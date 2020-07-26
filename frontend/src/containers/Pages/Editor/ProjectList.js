e645107e7b1548c4cdb79f3003b89ef690409822
lilianshi00@gmail.com <lilianshi00@gmail.com>
2020-07-25 23:50:42 -0400

    better styling on prjolist

--- frontend/src/containers/Pages/Editor/ProjectList.js
+++ frontend/src/containers/Pages/Editor/ProjectList.js
@@ -4,11 +4,15 @@ import styles from './ProjectList.module.css';
 const ProjectList = (props)=>{
 
     const projects = props.projects;
-    const list = projects.map(p=>{return <div style = {style} key = {p._id}>{p.projID}</div>});
+    const list = projects.map(p=>{return <div className = {styles.ProjUnit} key = {p._id}>{p.projID}<br/>{p.projName}</div>});
     console.log(list)
     return(
-        <div style = {styleWrapper}>
+        <div className = {styles.Wrapper}>
+        <p style ={{fontWeight:"bold", fontSize: 24, display: 'inline-block'}}>Projects</p>
+
+        <div className = {styles.ListWrapper}>
             {list}
+        </div>
         </div>
         
     )
