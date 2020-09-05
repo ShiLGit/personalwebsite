4b2acec9f02bbcf3c9dfdf177c0ebaa89201259c
lilianshi00@gmail.com <lilianshi00@gmail.com>
2020-09-05 16:10:37 -0400

    hamburger and accordion adjusted for 320p viewport

--- frontend/src/components/Navigation/Drawer/DrawerElements/Hamburger.js
+++ frontend/src/components/Navigation/Drawer/DrawerElements/Hamburger.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './Hamburger.module.css';
 import GithubIcon from '../../../../pictures/github_icon.svg';
-
+import {NavLink} from 'react-router-dom';
 const Hamburger = (props)=>{
 
     return(
         <div className ={styles.Wrapper} >
-            LILIAN SHI
+            <NavLink to = "/">
+                <h1 className = {styles.Header}>LILIAN SHI</h1>
+
+            </NavLink>
             <a href="https://github.com/ShiLGit" target="_blank" rel="noopener noreferrer">
                 <img src={GithubIcon} className={styles.Image} alt="github link"/>
             </a>
