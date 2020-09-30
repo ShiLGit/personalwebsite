c5066478d70cdcede1b013c1b60362b2fd5f6f5b
lilianshi00@gmail.com <lilianshi00@gmail.com>
2020-09-29 22:03:03 -0400

    emailsender + email route working!!!

--- server/routers/emailRouter.js
+++ server/routers/emailRouter.js
@@ -14,72 +14,44 @@ emailRouter.route('/send').post(async (req, res)=>{
   //exit if invalid data
   if(!emailData.emailBody)
     return res.status(400).send({error: "Missing message body."});
-
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      type: "OAuth2",
-      user: process.env.EMAIL_USER,
-      clientId: process.env.OAUTH2_CLIENT_ID,
-      clientSecret: process.env.OAUTH2_CLIENT_SECRET,
-      refreshToken: process.env.OAUTH2_REFRESH_TOKEN,
-      accessToken: accessToken
-    }
-  });
-
-  const mailOptions = {
-    from: emailData.senderEmail?emailData.senderEmail: process.env.EMAIL_USER,
-    to: process.env.EMAIL_USER,
-    subject: `Persweb email from ${emailData.senderName}`,
-    text: emailData.emailBody + ((emailData.senderEmail && senderEmail !== "")? `\n\nSender email: ${emailData.senderEmail}`:null)
-  };
-  let emailResponse={success:false, response: null};
-  transporter.sendMail(mailOptions, (err, res)=>{
-    if(err){
-      emailResponse.respsonse=err;
-      console.log(err);
-      //      return res.status(500).send(err);
-    }
-    if(res){
-      console.log("res.")
-      emailResponse ={success: true, response: res}
-    }
-    console.log("wat")
-    res.send({error: "Did not receive success affirmation - email probably unsuccessfull..."});
-
-  })
-});
-/*
-async function main() {
-  let testAccount = await nodemailer.createTestAccount();
-  console.log("******************\n\n\n\n\n\n\n\n" + process.env.OAUTH2_REFRESH_TOKEN)
   
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      type: "OAuth2",
-      user: process.env.EMAIL_USER,
-      clientId: process.env.OAUTH2_CLIENT_ID,
-      clientSecret: process.env.OAUTH2_CLIENT_SECRET,
-      refreshToken: process.env.OAUTH2_REFRESH_TOKEN,
-      accessToken: accessToken
-    }
-  });
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: process.env.EMAIL_USER,
-    subject: "test",
-    html: "<h1>hallo wurlsd</h1>"
-  };
+  //SEND EMAIL
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.EMAIL_USER,
+        clientId: process.env.OAUTH2_CLIENT_ID,
+        clientSecret: process.env.OAUTH2_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH2_REFRESH_TOKEN,
+        accessToken: accessToken
+      }
+    });
+    
+    const mailOptions = {
+      from: emailData.senderEmail?emailData.senderEmail: process.env.EMAIL_USER,
+      to: process.env.EMAIL_USER,
+      subject: `Persweb email from ${emailData.senderName}`,
+      text: emailData.emailBody + ((emailData.senderEmail && emailData.senderEmail !== "")? `\n\nSender email: ${emailData.senderEmail}`:null)
+    };
+    //let emailResponse={success:false, response: null};
+    transporter.sendMail(mailOptions, (err, emailRes)=>{
+      if(err){
+        emailResponse.respsonse=err;
+        console.log("ERROR:", err);
+        res.send({error: "Email did not send successfully. (INTERNAL ERROR - DARN!!)"})
+      }
+      if(emailRes){
+        console.log("res.")
+        //emailResponse ={success: true, response: res}
+        res.send({success: "Email sent successfully."});
+      }
+    });  
+ 
+ 
+});
 
-  transporter.sendMail(mailOptions, (err, res)=>{
-    if(err)
-      console.log(err);
-    if(res)
-      console.log(res)
-  })
+async function sendEmail(emailData){  
+ 
 }
-
-main().catch(console.error);*/
-
 module.exports = emailRouter;
