54885ab2b08f4ef41b605cf5f24638af5a5e69d1
lilianshi00@gmail.com <lilianshi00@gmail.com>
2020-11-29 21:20:18 -0500

    fix wonky projviewer loading?

--- frontend/src/containers/Pages/Email/Email.js
+++ frontend/src/containers/Pages/Email/Email.js
@@ -1,91 +1,104 @@
-import React, {Component} from 'react';
-import axios from 'axios';
-import styles from './EmailForm.module.css';
-import Loader from '../../../components/UI/Loader';
-class Email extends Component{
-    state = {
-        emailData: {
-            senderName: "",
-            senderEmail: "",
-            emailBody: ""
-        },
-        loading: false
-    }
-    onNameChange=(e)=>{
-        const newState = {...this.state};
-        newState.emailData.senderName = e.target.value;
-        this.setState(newState);
-    }
-    onEmailChange=(e)=>{
-        const newState = {...this.state};
-        newState.emailData.senderEmail = e.target.value;
-        this.setState(newState);
-    }
-    onMessageChange=(e)=>{
-        const newState = {...this.state};
-        newState.emailData.emailBody = e.target.value;
-        this.setState(newState);
-    }
-    sendEmail=(e)=>{
-        e.preventDefault();
-        this.setState({loading:true});
+import React, { Component } from "react";
+import axios from "axios";
+import styles from "./EmailForm.module.css";
+import Loader from "../../../components/UI/Loader";
+class Email extends Component {
+  state = {
+    emailData: {
+      senderName: "",
+      senderEmail: "",
+      emailBody: "",
+    },
+    loading: false,
+  };
+  onNameChange = (e) => {
+    const newState = { ...this.state };
+    newState.emailData.senderName = e.target.value;
+    this.setState(newState);
+  };
+  onEmailChange = (e) => {
+    const newState = { ...this.state };
+    newState.emailData.senderEmail = e.target.value;
+    this.setState(newState);
+  };
+  onMessageChange = (e) => {
+    const newState = { ...this.state };
+    newState.emailData.emailBody = e.target.value;
+    this.setState(newState);
+  };
+  sendEmail = (e) => {
+    e.preventDefault();
+    this.setState({ loading: true });
 
-        axios.post('http://localhost:5000/email/send', this.state.emailData)
-        .then(res=>{
-            this.setState({loading: false});
+    axios
+      .post("http://localhost:5000/email/send", this.state.emailData)
+      .then((res) => {
+        this.setState({ loading: false });
 
-            //Server sends err msg
-            if(res.data.error)
-                alert(res.data.error);
-            else
-                alert(res.data.success);
-        })
-        .catch(err=>{
-            this.setState({loading: false});
-            alert(err);
-        })
-        .finally(()=>{
-            this.setState({loading:false});
-            const newEmailData = {
-                senderName: "",
-                senderEmail: "",
-                emailBody: ""
-            }
+        //Server sends err msg
+        if (res.data.error) alert(res.data.error);
+        else alert(res.data.success);
+      })
+      .catch((err) => {
+        this.setState({ loading: false });
+        alert(err);
+      })
+      .finally(() => {
+        this.setState({ loading: false });
+        const newEmailData = {
+          senderName: "",
+          senderEmail: "",
+          emailBody: "",
+        };
 
-            this.setState({emailData: newEmailData});
-        })
-    }
-    render(){
-        return(
-        this.state.loading?
-        <div>
-            <br/>
-            <br/>
-            <br/>            <br/>
-            <br/>
-            <br/>
-            <Loader/>
-        </div>:
-        <div className={styles.Wrapper}>
-            <h1 className={styles.Heading}>SEND AN EMAIL</h1>
-            <form className={styles.Form} onSubmit={this.sendEmail}>
-                <label>Your Name</label>
-                <input type = "text" required value={this.state.emailData.senderName} onChange={this.onNameChange}></input>
-                <br/>
-                <label>Your Email (Optional, For Replies)</label>
-                <input type = "email" value={this.state.emailData.senderEmail}onChange={this.onEmailChange}></input>
-                <br/>
-                <br/>
-                <label>Message</label>
-                <textarea required value={this.state.emailData.emailBody}className={styles.TextArea} rows={10} onChange={this.onMessageChange}></textarea>
-                <br/>
+        this.setState({ emailData: newEmailData });
+      });
+  };
+  render() {
+    return this.state.loading ? (
+      <div>
+        <br />
+        <br />
+        <br /> <br />
+        <br />
+        <br />
+        <Loader />
+      </div>
+    ) : (
+      <div className={styles.Wrapper}>
+        <h1 className={styles.Heading}>SEND AN EMAIL</h1>
+        <form className={styles.Form} onSubmit={this.sendEmail}>
+          <label>Your Name</label>
+          <input
+            type="text"
+            required
+            value={this.state.emailData.senderName}
+            onChange={this.onNameChange}
+          ></input>
+          <br />
+          <label>Your Email (Optional, For Replies)</label>
+          <input
+            type="email"
+            value={this.state.emailData.senderEmail}
+            onChange={this.onEmailChange}
+          ></input>
+          <br />
+          <br />
+          <label>Message</label>
+          <textarea
+            required
+            value={this.state.emailData.emailBody}
+            className={styles.TextArea}
+            rows={10}
+            onChange={this.onMessageChange}
+          ></textarea>
+          <br />
 
-                <input type="submit" value="Send Email"/>
-            </form>
-        </div>
-        )
-    }
-
+          <input type="submit" value="Send Email" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Email;
--- frontend/src/containers/Pages/ProjViewer/ProjViewer.js
+++ frontend/src/containers/Pages/ProjViewer/ProjViewer.js
@@ -1,56 +1,87 @@
-import React, {Component} from 'react';
-import styles from './ProjViewer.module.css';
-import Carousel from '../../../components/Carousel/Carousel';
-import {connect} from 'react-redux';
-import Loader from '../../../components/UI/Loader';
-import {Redirect} from 'react-router-dom';
-class ProjViewer extends Component{
-    state={
-        curProjID: this.props.match.params.projID,
-        curProj: null
-    }
-    componentDidMount(){
-        console.log(this.state.curProjID, this.props.projects.find(p=>p.projID === this.props.match.params.projID));
-    }
-    //update projviewer if url params changed/props.projects loaded in (previously undefined, usually just applies on component init)
-    componentDidUpdate(){
-        if(this.props.match.params.projID !== this.state.curProjID || !this.state.curProj){
-            const target = this.props.projects.find(p=>p.projID === this.props.match.params.projID);
-            if(!target && (this.state.curProjID !== 'null' && this.props.match.params.projID !=='null')){
-                alert("Project of identifier " + this.props.match.params.projID + " does not exist.")
-                this.setState({curProjID: "null"})
+import React, { Component } from "react";
+import styles from "./ProjViewer.module.css";
+import Carousel from "../../../components/Carousel/Carousel";
+import { connect } from "react-redux";
+import Loader from "../../../components/UI/Loader";
+import { Redirect } from "react-router-dom";
+class ProjViewer extends Component {
+	state = {
+		curProjID: this.props.match.params.projID,
+		curProj: null
+	};
+	componentDidMount() {
+		console.log(
+			this.state.curProjID,
+			this.props.projects.find((p) => p.projID === this.props.match.params.projID)
+		);
+		this.setState({
+			curProjID: this.props.match.params.projID,
+			curProj: this.props.projects.find((p) => p.projID === this.props.match.params.projID)
+		});
+	}
+	//fires every time url changes/proj props load in (ie on init)
+	componentDidUpdate() {
+		console.log("cdidupdate", this.props.match.params.projID !== this.state.curProjID, !this.state.curProj);
+		//update project being displayed; url changed while state curProjId = prev proj id
+		if (this.props.match.params.projID !== this.state.curProjID || !this.state.curProj) {
+			const target = this.props.projects.find((p) => p.projID === this.props.match.params.projID);
 
-            }
-            this.setState({curProjID: this.props.match.params.projID, curProj: this.props.projects.find(p=>p.projID === this.props.match.params.projID)});
-        }
-    }
+			//case 1: an existing project matches route id
+			if (target) {
+				this.setState({
+					curProjID: this.props.match.params.projID,
+					curProj: target
+				});
+			} else if (!target && this.props.projects.length > 0) {
+				//case 2: projects are loaded in but there's no match, load in proj#1 as default
+				this.setState({ curProjID: this.props.projects[0].projID, curProj: this.props.projects[0] });
+				this.props.history.push("/projects/" + this.props.projects[0].projID);
+				console.log(
+					"ProjViewer: no project matching id " + this.props.match.params.projID + ". Loading first project."
+				);
+			}
+		}
+	}
 
-    render(){
-        let toRender = (<div><br/><br/><br/><br/><br/><Loader/></div>);
-        if(this.state.curProj){
-            toRender = (<div className={styles.Wrapper}>
-                <div className={styles.TitleWrapper}>
-                    <h1>{this.state.curProj.projName}</h1>
-                </div>
-                
-                <img src={require('../../../pictures/proj/' + this.state.curProj.demoImageName )} className={styles.DemoImage}/>
-                <div className={styles.CarouselWrapper}>
-                    <Carousel category="all"/>
-                </div>
-            <div className = {styles.TextWrapper} dangerouslySetInnerHTML={{__html:this.state.curProj.bodyMarkup}}>
-            </div>
-        </div>)
-        }else if(this.state.curProjID || !this.state.curProjID){
-            if(this.props.projects.length > 0)
-                toRender = <Redirect to = {"/projects/" + this.props.projects[0].projID}/>
-        }
-        return(toRender);
-    }
+	render() {
+		let toRender = (
+			<div>
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<Loader />
+			</div>
+		);
+		console.log("rendering...", this.state);
+		if (this.state.curProj) {
+			toRender = (
+				<div className={styles.Wrapper}>
+					<div className={styles.TitleWrapper}>
+						<h1>{this.state.curProj.projName}</h1>
+					</div>
 
+					<img
+						src={require("../../../pictures/proj/" + this.state.curProj.demoImageName)}
+						className={styles.DemoImage}
+					/>
+					<div className={styles.CarouselWrapper}>
+						<Carousel category='all' />
+					</div>
+					<div
+						className={styles.TextWrapper}
+						dangerouslySetInnerHTML={{ __html: this.state.curProj.bodyMarkup }}
+					></div>
+				</div>
+			);
+		}
+		return toRender;
+	}
 }
-const stateToProps = (state)=>{
-    return{
-        projects: state.projReducer.projects
-    }
-}
+const stateToProps = (state) => {
+	return {
+		projects: state.projReducer.projects
+	};
+};
 export default connect(stateToProps)(ProjViewer);
