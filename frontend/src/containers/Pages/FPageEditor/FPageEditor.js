b04d22cc6bdbcd00b339018ccd7408e15e361879
lilianshi00@gmail.com <lilianshi00@gmail.com>
2020-12-06 21:06:13 -0500

    get dat dbl binding on fpageeditor

--- frontend/src/containers/Pages/FPageEditor/FPageEditor.js
+++ frontend/src/containers/Pages/FPageEditor/FPageEditor.js
@@ -4,24 +4,65 @@ import formStyle from "../FormStyles.module.css";
 import { Redirect } from "react-router-dom";
 
 class FPageEditor extends Component {
+	state = {
+		webDevTS: "",
+		schoolTS: "",
+		otherTS: "",
+		bio: ""
+	};
+	changeStateField = (e, field) => {
+		const stateUpdate = {};
+		stateUpdate[field] = e.target.value;
+		this.setState(stateUpdate);
+	};
+	submitHandler = () => {};
 	render() {
-		let toRender = <Redirect to='unauthorized' />;
-		if (this.props.token)
+		//let toRender = <Redirect to='unauthorized' />;
+		let toRender;
+		if (1)
 			toRender = (
 				<>
 					<form className={formStyle.Form} style={{ marginTop: "80px" }}>
 						<h1>Edit Front Page</h1>
 						<label className={formStyle.InputLabel}>Tech Stack: Web Dev</label>
-						<input type='text'></input>
+						<input
+							type='text'
+							value={this.state.webDevTS}
+							onChange={(e) => {
+								this.changeStateField(e, "webDevTS");
+							}}
+							required
+						></input>
 
 						<label className={formStyle.InputLabel}>Tech Stack: School</label>
-						<input type='text'></input>
+						<input
+							type='text'
+							value={this.state.schoolTS}
+							onChange={(e) => {
+								this.changeStateField(e, "schoolTS");
+							}}
+							required
+						></input>
 
 						<label className={formStyle.InputLabel}>Tech Stack: Other</label>
-						<input type='text'></input>
-
-						<label className={formStyle.InputLabel}>Bio</label>
-						<input type='text'></input>
+						<input
+							type='text'
+							value={this.state.otherTS}
+							onChange={(e) => {
+								this.changeStateField(e, "otherTS");
+							}}
+							required
+						></input>
+						<br />
+						<label>Bio</label>
+						<input
+							type='textarea'
+							value={this.state.bio}
+							onChange={(e) => {
+								this.changeStateField(e, "bio");
+							}}
+							required
+						></input>
 
 						<input type='submit' value='Save'></input>
 					</form>
