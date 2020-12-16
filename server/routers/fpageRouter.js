35af8eb59f0502cc9527e66aefe3a3d791ef5316
lilianshi00@gmail.com <lilianshi00@gmail.com>
2020-12-15 21:08:07 -0500

    TODO: FINISH ROUTES, HOOK UP TO REDUX, FIX NAVLINK BUG (EZ C STACKOVERFLOW). SCHEMA VALIDATION IS NOT WORKING ON SET ROUTE OTHERWISE IT'S DONE

--- server/routers/fpageRouter.js
+++ server/routers/fpageRouter.js
@@ -1,23 +1,42 @@
 const fpageRouter = require("express").Router();
-const { findOne } = require("../models/FPageData");
+const { findOne, findByIdAndDelete } = require("../models/FPageData");
 const FPageData = require("../models/FPageData");
-fpageRouter.route("/getinfo").get(async (req, res) => {
-	const toSend = await FPageData.findOne();
-	res.send(toSend);
-});
+
 fpageRouter.route("/setinfo").post(async (req, res) => {
 	if (!req.body) {
 		return res.status(400).send({ error: "Missing body." });
 	}
-	console.log(req.body);
+	let returnStatusCode = 200;
+	let returnData = { success: "New frontpage data saved successfully." };
 	const saveData = new FPageData({ ...req.body });
-	saveData.save((err) => {
-		if (err) {
-			console.log(err);
-			return res.status(400).send({ error: "Error saving request body. Check if request is valid?." });
-		} else {
-			res.status(200).send({ success: "SAVED DAWG" });
+
+	try {
+		//step 1: existing entries? make sure there won't be more than 1
+		const existingData = await FPageData.find();
+		if (existingData.length >= 1) {
+			console.log("in block #1");
+			for (let i = 0; i < existingData.length; i++) {
+				console.log("deleting", existingData[i].bio);
+				await FPageData.findByIdAndDelete(existingData[i]._id);
+			}
 		}
-	});
+
+		//save data
+		await saveData.save((err) => {
+			if (err) returnData = { error: "Error saving request body. Check if request is valid?." };
+		});
+	} catch (e) {
+		console.log(e);
+		returnStatusCode = 200;
+	} finally {
+		res.status(returnStatusCode).send(returnData);
+	}
+});
+
+fpageRouter.route("/getinfo").get(async (req, res) => {
+	console.log("wtf");
+	const data = await FPageData.find();
+	console.log(data.length);
+	res.send({ wot: "WAZZZZZZA" });
 });
 module.exports = fpageRouter;
--- server/routers/projRouter.js
+++ server/routers/projRouter.js
@@ -1,105 +1,95 @@
-const path = require('path');
-const projRouter = require('express').Router();
-const ProjText = require('../models/ProjText');
-const auth = require('../middleware/auth');
-const multer = require('multer');
+const path = require("path");
+const projRouter = require("express").Router();
+const ProjText = require("../models/ProjText");
+const auth = require("../middleware/auth");
+const multer = require("multer");
 const glob = require("glob");
-const fs = require('fs');
-
+const fs = require("fs");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../../frontend/src/pictures/proj'))
-      },
-      filename: function (req, file, cb) {
-        cb(null, file.originalname);
-      }
+	destination: function (req, file, cb) {
+		cb(null, path.join(__dirname, "../../frontend/src/pictures/proj"));
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname);
+	}
 });
 
-const upload = multer({storage});
+const upload = multer({ storage });
 
 //get all project (JSON) data in database
-projRouter.route('/init').get(async (req,res)=>{
-  try{
-    const allProj = await ProjText.find({});
-    return  res.status(200).send({projects: allProj});
-  }catch(e){
-    console.log("Database error...", e);
-    return res.status(500).send(e);
-  }
-
+projRouter.route("/init").get(async (req, res) => {
+	try {
+		const allProj = await ProjText.find({});
+		return res.status(200).send({ projects: allProj });
+	} catch (e) {
+		console.log("Database error...", e);
+		return res.status(500).send(e);
+	}
 });
 
 //docID = _id in mongodb, projID = projId property of projText objects
 //USE AUTH MIDDLEWARE ONCE DONE TESTING
-projRouter.route('/delete/:docID/:projID').delete(auth,async (req, res)=>{
-   let err = null;
-  const deleted = await ProjText.findByIdAndDelete(req.params.docID)
-                  .catch(e=>{
-                    console.log(e);
-                    err = e;
-                  })
-  
-  //delete all pictures in folder with projID prestr
-  glob(path.join(__dirname, '../../frontend/src/pictures/proj/' +req.params.projID+ "*"), (err, files)=>{
-    if(err){
-      console.log(err);
-    }else{
-      files.forEach(f=>{
-         
-          if(f.includes(req.params.projID + "_")){
-            console.log("ATTEMPTING TO UNLINK " + f);
-            fs.unlink(f, err=>{
-              if(err)
-                console.log("\nFILE DELETION ERROR: \n" + err);
-            })
-          }
-          
-      })
-    }
-  });
-  if(!err)
-    return res.status(200).send({success: "Successfully deleted " + deleted.projName, deleted});
-      
-  res.status(400).send(e);
+projRouter.route("/delete/:docID/:projID").delete(auth, async (req, res) => {
+	let err = null;
+	const deleted = await ProjText.findByIdAndDelete(req.params.docID).catch((e) => {
+		console.log(e);
+		err = e;
+	});
+
+	//delete all pictures in folder with projID prestr
+	glob(path.join(__dirname, "../../frontend/src/pictures/proj/" + req.params.projID + "*"), (err, files) => {
+		if (err) {
+			console.log(err);
+		} else {
+			files.forEach((f) => {
+				if (f.includes(req.params.projID + "_")) {
+					console.log("ATTEMPTING TO UNLINK " + f);
+					fs.unlink(f, (err) => {
+						if (err) console.log("\nFILE DELETION ERROR: \n" + err);
+					});
+				}
+			});
+		}
+	});
+	if (!err) return res.status(200).send({ success: "Successfully deleted " + deleted.projName, deleted });
+
+	res.status(400).send(e);
 });
 
-projRouter.route('/addpic').post(auth, upload.array('pictures', 2), async (req,res)=>{
-    console.log("img added");
-    res.status(200).send({success: "HI!"});
-
+projRouter.route("/addpic").post(auth, upload.array("pictures", 2), async (req, res) => {
+	console.log("img added");
+	res.status(200).send({ success: "HI!" });
 });
 
-projRouter.route('/addtext').post(auth, async (req,res)=>{
-  console.log('...payload??', req.body);
-  let newProj;
-  const duplicate = await ProjText.findOne({projID: req.body.projID});
-  if(!duplicate){
-    newProj = new ProjText({...req.body});
-    console.log("SAVING", newProj);
-    newProj.save((err)=>{
-      if(err){
-        console.log(err)
-        return res.status(400).send({error: "Error saving " +newProj.projName});
-      }
-      
-      res.status(200).send({success: newProj.projName + " saved successfully.", saved: newProj});      
-    });
-  }else{
-    return res.status(401).send({error: "Project with given ProjID already exists. Try selecting EDIT option."});
-  }
-  
+projRouter.route("/addtext").post(auth, async (req, res) => {
+	console.log("...payload??", req.body);
+	let newProj;
+	const duplicate = await ProjText.findOne({ projID: req.body.projID });
+	if (!duplicate) {
+		newProj = new ProjText({ ...req.body });
+		console.log("SAVING", newProj);
+		newProj.save((err) => {
+			if (err) {
+				console.log(err);
+				return res.status(400).send({ error: "Error saving " + newProj.projName });
+			}
 
+			res.status(200).send({ success: newProj.projName + " saved successfully.", saved: newProj });
+		});
+	} else {
+		return res.status(401).send({ error: "Project with given ProjID already exists. Try selecting EDIT option." });
+	}
 });
 
 //update project text
-projRouter.route('/edittext/:projIDName').put(auth, async (req, res)=>{
-  try{
-    const project = await ProjText.findOneAndReplace({projID: req.params.projIDName}, req.body, {new: true});
-    return res.send({success: req.body.projName + " updated successfully.", updated: project});
-  }catch(e){
-    res.status(400).send(e);
-  }
+projRouter.route("/edittext/:projIDName").put(auth, async (req, res) => {
+	try {
+		const project = await ProjText.findOneAndReplace({ projID: req.params.projIDName }, req.body, { new: true });
+		return res.send({ success: req.body.projName + " updated successfully.", updated: project });
+	} catch (e) {
+		res.status(400).send(e);
+	}
 });
 
 module.exports = projRouter;
